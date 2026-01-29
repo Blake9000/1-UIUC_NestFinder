35470dd3a5cf866517809fa4369e9443c4a37bf2
Blake Mackin <bmackin2@illinois.edu>
2026-01-28 22:06:02 -0600

    Added models to admin

--- apartments/admin.py
+++ apartments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Apartment
 # Register your models here.
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ("price","address")
--- subleases/admin.py
+++ subleases/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Sublease
 # Register your models here.
+
+@admin.register(Sublease)
+class SubleaseAdmin(admin.ModelAdmin):
+    list_display = ("price","address")
