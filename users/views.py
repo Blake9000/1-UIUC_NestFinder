0ffcf984e91072debca083da39197a2b7e7e1eed
Blake9000 <blakealmackin@gmail.com>
2026-02-06 15:24:19 -0600

    Added basic login page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def about(request):
     return render(request, 'users/about.html')
+
+def login(request):
+    html = render(request, 'users/login.html')
+    return HttpResponse(html, content_type='text/html')
