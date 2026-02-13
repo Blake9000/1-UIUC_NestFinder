ea4fd37671e24b891172b87a382c7dfaa28b388c
hne43 <lh32@illinois.edu>
2026-02-12 19:57:40 -0600

    cache busting

--- NestFinder/settings/production.py
+++ NestFinder/settings/production.py
@@ -1,9 +1,14 @@
 # Paste in and as: illinois/settings/production.py
 # We will first import everything from base.py (our new settings.py file that we renamed to base.py)
 #==================================================================================
+from django.conf import STATICFILES_STORAGE_ALIAS
+
 from .base import *
 
 DEBUG = False
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Replace it with your name:
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
