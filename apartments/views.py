b700c6aaca17b71f14c54df463e2472666dbfd4c
APosadasZ <alexz8@illinois.edu>
2026-02-12 19:12:55 -0600

    matplotlib changes

--- apartments/views.py
+++ apartments/views.py
@@ -5,7 +5,10 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from apartments.models import Apartment
-
+import json
+from io import BytesIO
+import matplotlib.pyplot as plt
+from django.urls import reverse
 
 # Create your views here.
 
@@ -46,30 +49,17 @@ class ApartmentsAPI(View):
         return JsonResponse({"ok":True, "data":data})
 
 
-# ------ Chart API & Matplotlib png chart --------
-def ApartmentsPriceAPI(request):
-    rows = Apartment.objects.all('name', 'price').order_by('-price')
-    return JsonResponse({"ok":True, "data":rows})
 
-import json, urllib.request
-from io import BytesIO
-import matplotlib.pyplot as plt
-from django.urls import reverse
 
 def apartment_price_chart_png(request):
     api_url = request.build_absolute_uri(reverse('apartments:price-chart'))
 
     #fetching json data from the API
-    with urllib.request.urlopen(api_url) as resp:
-        payload = json.load(resp)
-
-    #extracring rows
-    rows = payload.get('results', [])
+    rows = Apartment.objects.all('name', 'price').order_by('-price')
 
     labels = [r['name'] for r in rows]
     prices = [r['price'] for r in rows]
 
-    # Creating the bar chart (this is temporary)
     fig, ax = plt.subplots(figsize=(10,10), dpi=200 )
     ax.bar(labels, prices)# I could add color
     ax.set_title('Apartment Price Chart Comparison')
@@ -77,11 +67,9 @@ def apartment_price_chart_png(request):
     ax.set_xticks(labels, rotation=90, ha='right')
     fig.tight_layout()
 
-    # Converting chart to a PNG in RAM
     buf = BytesIO()
     fig.savefig(buf, format='png')
     plt.close(fig)
     buf.seek(0)
     return HttpResponse(buf.getvalue(), content_type='image/png')
-
 
