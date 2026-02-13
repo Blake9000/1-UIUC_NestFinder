4d167c6bda955954e8e35aa00bee1fd569a20211
APosadasZ <alexz8@illinois.edu>
2026-02-12 18:58:10 -0600

    test/chart path

--- apartments/urls.py
+++ apartments/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
      path('', ListingView.as_view(), name='listing'),
      path('detail/<int:pk>/', ListingDetailView.as_view(), name='listing_detail'),
      path('apartments/api/',ApartmentsAPI.as_view(), name='apartments_api'),
+     path('test/chart', apartment_price_chart_png, name='test-chart'),
 ]
