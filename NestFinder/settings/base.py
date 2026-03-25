382de7a86137facb3365604299a6acf2f0c15caa
Blake Mackin <bmackin2@illinois.edu>
2026-03-25 10:32:10 -0500

    Added white noise

--- NestFinder/settings/base.py
+++ NestFinder/settings/base.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'NestFinder.urls'
